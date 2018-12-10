4d7aca21f66625c3ccafd43db1a65219c2fee5bf
Tansy Roberts <tansymiriam@gmail.com>
2018-12-10 20:18:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name)
+{ drivers.push('Ralph');
+}
+function destructivelyPrependDriver(name)
+{ drivers.unshift('Bob');
+}
+function destructivelyRemoveLastDriver(name)
+{drivers.pop()}
+
+function destructivelyRemoveFirstDriver(name)
+{drivers.shift()}
+
+function appendDriver(name) {
+const copyOfDrivers = drivers.slice()
+drivers.push('Broom')
+}
