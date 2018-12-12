eee45665a7be465a748af36d0bb61463be571e33
Tansy Roberts <tansymiriam@gmail.com>
2018-12-12 02:55:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name)
+{ drivers.push('Ralph');
+}
+function destructivelyPrependDriver(name)
+{ drivers.unshift('Bob');
+}
+function destructivelyRemoveLastDriver(name)
+{drivers.pop()}
+
+function destructivelyRemoveFirstDriver(name)
+{drivers.shift()}
+
+function appendDriver(name) {
+const appendDriver = [...drivers, "Broom"];
+return appendDriver
+}
+
+function prependDriver(name) {
+const prependDriver = ["Arnold",...drivers];
+return prependDriver
+}
+function removeLastDriver(name) {
+  const lastDriver = drivers.slice()
+}
